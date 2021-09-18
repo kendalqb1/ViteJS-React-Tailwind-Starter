38752548b74a2055e2f1889c1ccc8a2db393f87b
kendalqb1 <kendalqb1@gmail.com>
2021-09-17 19:40:28 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import "tailwindcss/tailwind.css"
+
+function App() {
+  return (
+    <div className="flex justify-center mt-52">
+      <h1 className="text-9xl">Tailwind con Vite js React</h1>    
+    </div>
+  )
+}
+
+export default App
